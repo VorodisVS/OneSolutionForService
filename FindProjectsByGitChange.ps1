af837d4ed9049cd3e591db08733a661b43c422d5
VorodisVS <vorodisvs@gmail.com>
2021-11-01 22:52:11 +0300

    32

--- FindProjectsByGitChange.ps1
+++ FindProjectsByGitChange.ps1
@@ -1,5 +1,5 @@
-$changedFiles = git diff --name-only feature/branch2 master
-$changedProj = $changedFiles | Foreach-Object { $_.Split("/")[1] }
+$changedFiles = git diff --name-only featue/branch2 master
+$changedProj = $changedFiles | Foreach-Object { $_.Split("/")[1] } | Where-Object {$_ -ne $null } | Where-Object { $_.Trim().Length -gt 0 }
 
 $slnFiles = Get-ChildItem -Recurse -Include "*.sln"
 
