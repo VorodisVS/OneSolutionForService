76249281e69728128eb1b16246c420cb67c2343f
VorodisVS <vorodisvs@gmail.com>
2021-11-02 12:34:45 +0300

    automatic current branch detect

--- FindProjectsByGitChange.ps1
+++ FindProjectsByGitChange.ps1
@@ -1,4 +1,11 @@
-$changedFiles = git diff --name-only featue/branch2 master
+[CmdletBinding()]
+
+Param (
+[Parameter (Mandatory=$true, Position=1)]
+[string[]]$TargetBranch
+)
+$curBranch = git branch --show-current
+$changedFiles = git diff --name-only $curBranch $TargetBranch
 $changedProj = $changedFiles | Foreach-Object { $_.Split("/")[1] } | Where-Object {$_ -ne $null } | Where-Object { $_.Trim().Length -gt 0 }
 
 $slnFiles = Get-ChildItem -Recurse -Include "*.sln"
